6ac34a84da75ec0b582a2beee0001d13960413d7
Alvaro Morales <alvarom@mit.edu>
2013-04-28 03:18:56 -0400

    Updated initiatives

--- survey/urls.py
+++ survey/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
                        url(r'^$', TemplateView.as_view(template_name='index.html')),
                        url(r'^survey/$', TemplateView.as_view(template_name='survey_index.html')),
                        url(r'^position/$', TemplateView.as_view(template_name='position.html')),
+                       url(r'^initiatives/$', TemplateView.as_view(template_name='initiatives.html')),
 )
